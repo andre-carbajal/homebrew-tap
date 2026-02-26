8412f2e31233b4190eaa025f84ef46fcf026fc43
jreleaserbot <jreleaser@kordamp.org>
2026-02-26 20:25:17 +0000

    naviger v1.12.1

--- Formula/naviger.rb
+++ Formula/naviger.rb
@@ -1,4 +1,4 @@
-# Generated with JReleaser 1.22.0 at 2026-02-26T19:18:33.816196205Z
+# Generated with JReleaser 1.22.0 at 2026-02-26T20:25:04.941138412Z
 
 class Naviger < Formula
   desc "Modern Minecraft Server Manager"
@@ -8,15 +8,15 @@ class Naviger < Formula
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/andre-carbajal/Naviger/releases/download/v1.12.1/Naviger-1.12.1-linux.zip"
-    sha256 "1635960d4c4eff6a4980a5a8c8b663f23e82990419185f0e305de09128c11492"
+    sha256 "bedc9a018261e75846e42fb964bb4a68b99dc381c7ce366ccd320627b7a67b6f"
   end
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/andre-carbajal/Naviger/releases/download/v1.12.1/Naviger-1.12.1-macos.zip"
-    sha256 "4b6dfa6d8b6ee6e86c333014d580117f1b491283536879c1ea65a2edaae8bb52"
+    sha256 "7e238071c4308fa913bf25cdf459203a1eef9f292de95d8de52038dff07c7c12"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/andre-carbajal/Naviger/releases/download/v1.12.1/Naviger-1.12.1-macos.zip"
-    sha256 "4b6dfa6d8b6ee6e86c333014d580117f1b491283536879c1ea65a2edaae8bb52"
+    sha256 "7e238071c4308fa913bf25cdf459203a1eef9f292de95d8de52038dff07c7c12"
   end
 
 
