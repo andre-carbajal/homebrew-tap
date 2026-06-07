19159e036155dc79704a5b4e0bb55a9a7fe4488f
jreleaserbot <jreleaser@kordamp.org>
2026-06-07 02:26:40 +0000

    depanalyzer v2.0.3-rc.1

--- Formula/depanalyzer.rb
+++ Formula/depanalyzer.rb
@@ -1,14 +1,14 @@
-# Generated with JReleaser 1.24.0 at 2026-05-08T03:59:08.991510308Z
+# Generated with JReleaser 1.24.0 at 2026-06-07T02:26:27.285088919Z
 
 class Depanalyzer < Formula
   desc "Dependency Analyzer CLI"
   homepage "https://github.com/UPT-FAING-EPIS/proyecto-si784-2026-i-u1-analizador-de-dependencias"
-  version "2.0.2"
+  version "2.0.3-rc.1"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/UPT-FAING-EPIS/proyecto-si784-2026-i-u1-analizador-de-dependencias/releases/download/v2.0.2/depanalyzer-macos-arm64.zip"
-    sha256 "4e5d77d80b8ead3d1bf79414a926a7725daf1c0a7d0c4a578642bbbab3051f4a"
+    url "https://github.com/UPT-FAING-EPIS/proyecto-si784-2026-i-u1-analizador-de-dependencias/releases/download/v2.0.3-rc.1/depanalyzer-macos-arm64.zip"
+    sha256 "db562e858dd5e4b2fc8b7f097ef6cd6f08d46d703ab4a172a22ad625833e1461"
   end
 
 
@@ -19,6 +19,6 @@ class Depanalyzer < Formula
 
   test do
     output = shell_output("#{bin}/depanalyzer --version")
-    assert_match "2.0.2", output
+    assert_match "2.0.3-rc.1", output
   end
 end
